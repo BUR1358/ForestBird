ffb41e70f1fb57b2273d07cd086bc79251745dba
Alexandr Blinov <bur1358@yandex.ru>
2022-05-30 17:05:12 +0300

    save progress

--- app/src/main/java/com/example/forestbird/BaseObject.java
+++ app/src/main/java/com/example/forestbird/BaseObject.java
@@ -1,16 +1,18 @@
 package com.example.forestbird;
 
 import android.graphics.Bitmap;
+import android.graphics.Rect;
 
 public class BaseObject {
     protected float x, y;
     protected int width, height;
+    protected Rect rect;
     protected Bitmap bm;
 
     public BaseObject() {
     }
 
-    public BaseObject(float x, float y, int width, int height, Bitmap bm) {
+    public BaseObject(float x, float y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -55,5 +57,13 @@ public class BaseObject {
 
     public void setBm(Bitmap bm) {
         this.bm = bm;
+    }
+
+    public Rect getRect() {
+        return new Rect((int) this.x, (int) this.y, (int) this.x + this.width, (int) this.y + this.height);
+    }
+
+    public void setRect(Rect rect) {
+        this.rect = rect;
     }
 }
